38b3513a8d97befeab101e60543feeb402a839d4
miharyjoel <miharyjoel@gmail.com>
2024-02-22 14:55:15 +0300

    format: black-and-white controller

--- src/main/java/hei/school/sarisary/endpoint/rest/controller/ImageProcessingController.java
+++ src/main/java/hei/school/sarisary/endpoint/rest/controller/ImageProcessingController.java
@@ -2,17 +2,14 @@ package hei.school.sarisary.endpoint.rest.controller;
 
 import hei.school.sarisary.file.BucketComponent;
 import hei.school.sarisary.service.ImageProcessingService;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
 
 @RestController
 @AllArgsConstructor
@@ -20,23 +17,24 @@ import java.util.Map;
 public class ImageProcessingController {
   private BucketComponent bucketComponent;
   private ImageProcessingService imageProcessingService;
+
   @PutMapping("/{id}")
-  public ResponseEntity<String> convertToBlackAndWhite(@PathVariable String id,
-                                                       @RequestParam("image") MultipartFile imageFile) {
+  public ResponseEntity<String> convertToBlackAndWhite(
+      @PathVariable String id, @RequestParam("image") MultipartFile imageFile) {
     return imageProcessingService.uploadTransformImage(id, imageFile);
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<Object> getTransformedImages(@PathVariable String id) {
     try {
-      String originalUrl = bucketComponent.presign("original/" + id, Duration.ofMinutes(5)).toString();
-      String transformedUrl = bucketComponent.presign("black-and-white/" + id, Duration.ofMinutes(5)).toString();
-
+      String originalUrl =
+          bucketComponent.presign("original/" + id, Duration.ofMinutes(5)).toString();
+      String transformedUrl =
+          bucketComponent.presign("black-and-white/" + id, Duration.ofMinutes(5)).toString();
 
       Map<String, String> responseBody = new HashMap<>();
       responseBody.put("original_url", originalUrl);
       responseBody.put("transformed_url", transformedUrl);
-
 
       return ResponseEntity.ok(responseBody);
     } catch (Exception e) {
@@ -44,5 +42,4 @@ public class ImageProcessingController {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
   }
-
 }
--- src/main/java/hei/school/sarisary/service/ImageProcessingService.java
+++ src/main/java/hei/school/sarisary/service/ImageProcessingService.java
@@ -1,18 +1,17 @@
 package hei.school.sarisary.service;
 
 import hei.school.sarisary.file.BucketComponent;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
+import javax.imageio.ImageIO;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @AllArgsConstructor
@@ -24,29 +23,26 @@ public class ImageProcessingService {
 
       String filename = UUID.randomUUID().toString() + ".png";
 
-
       File tempFile = File.createTempFile("temp", ".png");
       imageFile.transferTo(tempFile);
 
-
       bucketComponent.upload(tempFile, "original/" + filename);
 
-
       BufferedImage originalImage = ImageIO.read(tempFile);
-      BufferedImage bwImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+      BufferedImage bwImage =
+          new BufferedImage(
+              originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
       Graphics2D graphics = bwImage.createGraphics();
       graphics.drawImage(originalImage, 0, 0, null);
       graphics.dispose();
-
 
       File convertedFile = File.createTempFile("converted", ".png");
       ImageIO.write(bwImage, "png", convertedFile);
       bucketComponent.upload(convertedFile, "black-and-white/" + filename);
 
-
       tempFile.delete();
       convertedFile.delete();
-      
+
       return ResponseEntity.ok(id);
     } catch (IOException e) {
       e.printStackTrace();
