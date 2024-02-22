b247d492ab584b1e7c34221eeede2f19dc53d55c
miharyjoel <miharyjoel@gmail.com>
2024-02-22 14:53:22 +0300

    feat: add black-and-white controller

---
+++ src/main/java/hei/school/sarisary/endpoint/rest/controller/ImageProcessingController.java
@@ -0,0 +1,48 @@
+package hei.school.sarisary.endpoint.rest.controller;
+
+import hei.school.sarisary.file.BucketComponent;
+import hei.school.sarisary.service.ImageProcessingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/black-and-white")
+public class ImageProcessingController {
+  private BucketComponent bucketComponent;
+  private ImageProcessingService imageProcessingService;
+  @PutMapping("/{id}")
+  public ResponseEntity<String> convertToBlackAndWhite(@PathVariable String id,
+                                                       @RequestParam("image") MultipartFile imageFile) {
+    return imageProcessingService.uploadTransformImage(id, imageFile);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getTransformedImages(@PathVariable String id) {
+    try {
+      String originalUrl = bucketComponent.presign("original/" + id, Duration.ofMinutes(5)).toString();
+      String transformedUrl = bucketComponent.presign("black-and-white/" + id, Duration.ofMinutes(5)).toString();
+
+
+      Map<String, String> responseBody = new HashMap<>();
+      responseBody.put("original_url", originalUrl);
+      responseBody.put("transformed_url", transformedUrl);
+
+
+      return ResponseEntity.ok(responseBody);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+  }
+
+}
---
+++ src/main/java/hei/school/sarisary/service/ImageProcessingService.java
@@ -0,0 +1,56 @@
+package hei.school.sarisary.service;
+
+import hei.school.sarisary.file.BucketComponent;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class ImageProcessingService {
+  private BucketComponent bucketComponent;
+
+  public ResponseEntity<String> uploadTransformImage(String id, MultipartFile imageFile) {
+    try {
+
+      String filename = UUID.randomUUID().toString() + ".png";
+
+
+      File tempFile = File.createTempFile("temp", ".png");
+      imageFile.transferTo(tempFile);
+
+
+      bucketComponent.upload(tempFile, "original/" + filename);
+
+
+      BufferedImage originalImage = ImageIO.read(tempFile);
+      BufferedImage bwImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+      Graphics2D graphics = bwImage.createGraphics();
+      graphics.drawImage(originalImage, 0, 0, null);
+      graphics.dispose();
+
+
+      File convertedFile = File.createTempFile("converted", ".png");
+      ImageIO.write(bwImage, "png", convertedFile);
+      bucketComponent.upload(convertedFile, "black-and-white/" + filename);
+
+
+      tempFile.delete();
+      convertedFile.delete();
+      
+      return ResponseEntity.ok(id);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+  }
+}
